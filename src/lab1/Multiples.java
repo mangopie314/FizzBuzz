007182e78be4f400de2d86465f8e1d4a68a17b63
mangopie314 <mangopie314@gmail.com>
2026-09-15 19:47:27 -0400

    Fixing test cases

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,12 +2,16 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
+        System.out.println(multiples(1000, 3, 5));
+    }
+
+    static int multiples(int n, int a, int b) {
         int counter = 0;
-        for(int i = 1; i < 1000; i++) {
-            if(i % 3 == 0 || i % 5 == 0) {
-                counter ++;
+        for(int i = 1; i < n; i++) {
+            if(i % a == 0 || i % b == 0) {
+                counter++;
             }
         }
-        System.out.println(counter);
+        return counter;
     }
 }
